8a78b629da5fa584e8f45de088a8cc407728a0ea
anton <anton@cake.ru>
2021-01-02 22:53:04 +0300

    hero card properties component. Basic hero constants

---
+++ constants/hero.js
@@ -0,0 +1,19 @@
+export const HERO_STATUS_ALIVE = 'Alive'
+export const HERO_STATUS_DEAD = 'Dead'
+export const HERO_STATUS_UNKNOWN = 'unknown'
+
+export const HERO_STATUSES = [
+  HERO_STATUS_ALIVE,
+  HERO_STATUS_DEAD,
+  HERO_STATUS_UNKNOWN,
+]
+
+export const HERO_GENDER_MALE = 'Male'
+export const HERO_GENDER_FEMALE = 'Female'
+export const HERO_GENDER_UNKNOWN = 'unknown'
+
+export const HERO_GENDERS = [
+  HERO_GENDER_MALE,
+  HERO_GENDER_FEMALE,
+  HERO_GENDER_UNKNOWN,
+]
