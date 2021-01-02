aceb3e526290e1ff087e4ea4b0c3e2ae6e8dd323
anton <anton@cake.ru>
2021-01-02 21:52:29 +0300

    Describe base style

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['@/assets/styles/fonts/getschwifty.scss', '@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -33,7 +33,6 @@ export default {
     '@nuxtjs/axios',
     'nuxt-svg-loader',
   ],
-
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
     baseURL: 'https://rickandmortyapi.com/api',
